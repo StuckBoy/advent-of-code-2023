49c6e5c85954ea5b95ab31da84fac39fef8bbbdd
Zachary Stuck <zstuck96@gmail.com>
2023-12-01 15:48:32 -0500

    Create Placeholder for Puzzle One Part Two

---
+++ advent-of-code-2023/src/day/one/PuzzleTwo.java
@@ -0,0 +1,11 @@
+package day.one;
+
+public class PuzzleTwo {
+
+    public static void main(String[] args) {
+        /*
+        Do the same as Puzzle One, only now both integers *and* spelled single
+        digit numbers count
+         */
+    }
+}
