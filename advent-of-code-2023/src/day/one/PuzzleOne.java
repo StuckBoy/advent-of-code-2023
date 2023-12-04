4e1f9f6f69cbdd6125ca9438fd338f2ef0add492
Zachary Stuck <zstuck96@gmail.com>
2023-12-03 19:28:43 -0500

    Optimize Puzzle One for Puzzle Two

--- advent-of-code-2023/src/day/one/PuzzleOne.java
+++ advent-of-code-2023/src/day/one/PuzzleOne.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,13 +14,13 @@ public class PuzzleOne {
         List<String> input = obtainPuzzleInput();
         List<Integer> numbers = extractIntegers(input);
         int finalNumber = doMath(numbers);
-        //Output
         System.out.println("I think it's " + finalNumber);
     }
 
     private static List<String> obtainPuzzleInput() {
         List<String> input = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("PuzzleOneInput.txt"))) {
+        String inputFile = "PuzzleOneInput.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 input.add(line);
@@ -31,44 +32,37 @@ public class PuzzleOne {
     private static List<Integer> extractIntegers(List<String> input) {
         List<Integer> integers = new ArrayList<>();
         input.forEach(row -> {
-            String start = "";
-            String end = "";
-            //Toss into StringBuilder to simplify trims
-            StringBuilder sbRow = new StringBuilder(row);
-            while (!sbRow.isEmpty()) {
-                if (notValidYet(start)) {
-                    start = String.valueOf(sbRow.charAt(0));
-                }
-                if (notValidYet(end)) {
-                    end = String.valueOf(sbRow.charAt(sbRow.length() - 1));
-                }
-                try {
-                    Integer.parseInt(start);
-                    Integer.parseInt(end);
-                    //If neither threw the exception, we're done.
-                    break;
-                } catch (NumberFormatException ignored) {
-                }
-                if (notValidYet(start)) {
-                    sbRow.deleteCharAt(0);
-                }
-                if (notValidYet(end)) {
-                    sbRow.deleteCharAt(sbRow.length() - 1);
+            LinkedHashMap<Integer, Integer> digitPositionMap = getLiteralDigits(row);
+            //Javascript Math Time
+            Object[] positions = digitPositionMap.keySet().toArray();
+            List<Integer> positionIntegers = new ArrayList<>();
+            for (Object o : positions) {
+                if (o instanceof Integer) {
+                    positionIntegers.add((Integer) o);
                 }
             }
-            //Do Javascript Math
-            integers.add(Integer.valueOf(start.concat(end)));
+            Integer firstKey = positionIntegers.getFirst();
+            Integer lastKey = positionIntegers.getLast();
+            String start = String.valueOf(digitPositionMap.get(firstKey));
+            String end = String.valueOf(digitPositionMap.get(lastKey));
+            Integer finalNumber = Integer.valueOf(start.concat(end));
+            integers.add(finalNumber);
         });
         return integers;
     }
 
-    private static boolean notValidYet(String value) {
-        try {
-            Integer.valueOf(value);
-            return false;
-        } catch (NumberFormatException ex) {
-            return true;
+    private static LinkedHashMap<Integer, Integer> getLiteralDigits(String row) {
+        LinkedHashMap<Integer, Integer> digitPositionMap = new LinkedHashMap<>();
+        for (int i = 0; i < row.length(); i++) {
+            char character = row.charAt(i);
+            try {
+                int number = character - '0';
+                if (number >= 0 && number <= 9) {
+                    digitPositionMap.put(i, number);
+                }
+            } catch (NumberFormatException ignored) {}
         }
+        return digitPositionMap;
     }
 
     private static int doMath(List<Integer> input) {
