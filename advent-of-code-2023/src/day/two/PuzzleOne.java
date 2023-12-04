73fb4d4f84ef8582cd598f36cf9c3119c00f3cb2
Zachary Stuck <zstuck96@gmail.com>
2023-12-04 09:53:40 -0500

    Create Skeleton for Day Two Puzzle One

---
+++ advent-of-code-2023/src/day/two/PuzzleOne.java
@@ -0,0 +1,16 @@
+package day.two;
+
+import java.util.List;
+
+import helpers.Util;
+
+public class PuzzleOne {
+
+    private static final String inputFile = "DayTwoInput.txt";
+
+    public static void main(String[] args) {
+        List<String> input = Util.obtainPuzzleInput(inputFile);
+        int idSum = 0;
+        System.out.println("I think it's " + idSum);
+    }
+}
